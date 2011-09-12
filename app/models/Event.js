ad7d6a63e9e79d759ee07e4786447d502d2c0803
Pierre Spring <pierre@nelm.io>
2011-09-12 20:21:23 +0200

    Models in MVC file structure.

---
+++ app/models/Event.js
@@ -0,0 +1,37 @@
+Ext.regModel(
+    'techup.Event',
+    {
+        fields: [
+            {name: 'id', type: 'int'},
+            {name: 'name', type: 'string'},
+            {name: 'dateFrom', type: 'date', dateFormat: 'c'},
+            {name: 'dateTo', type: 'date', dateFormat: 'c'},
+            {name: 'location', type: 'string'},
+            {name: 'city', type: 'string'},
+            {name: 'canton', type: 'string'},
+            {name: 'lat', type: 'float'},
+            {name: 'lon', type: 'float'},
+            {name: 'description', type: 'string'},
+            {name: 'price', type: 'string'},
+            {name: 'link', type: 'string'},
+            {name: 'twitter_handle', type: 'string'},
+            {name: 'attendees', type: 'auto'},
+            {name: 'dateGroup', type: 'string'}
+        ]
+    }
+);
+
+Ext.regStore(
+    'techup.Events',
+    {
+        model : 'techup.Event',
+        proxy : {
+            type : 'ajax',
+            url : 'http://techup.ch/api/events/upcoming.json',
+            reader : {
+                type : 'json',
+                root : 'events'
+            }
+        }
+    }
+);
